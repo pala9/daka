78e7da14a4118a35d50ec4a4d978620e283e9fde
pala911 <43851628@qq.com>
2026-08-17 13:39:43 +0800

    fix: 注释不兼容百度定位 SDK 的 setIgnoreCacheException 调用，修复编译

--- app/src/main/java/com/zcshou/gogogo/MainActivity.java
+++ app/src/main/java/com/zcshou/gogogo/MainActivity.java
@@ -651,7 +651,8 @@ public class MainActivity extends BaseActivity implements SensorEventListener {
         //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
         locationOption.setIsNeedLocationPoiList(false);
         //可选，默认false，设置是否收集CRASH信息，默认收集
-        locationOption.setIgnoreCacheException(true);
+        // 当前 libs/BaiduLBS_Android.jar 中的 LocationClientOption 无此方法，导致编译失败，暂时注释
+        // locationOption.setIgnoreCacheException(true);
         //可选，默认false，设置是否开启Gps定位
         //locationOption.setOpenGps(true);
         locationOption.setOpenGnss(true);
